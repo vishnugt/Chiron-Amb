b89ae58de7e54db61e21cfc408c51aaa107bf7ab
Vishnu <vishnugt@hotmail.com>
2016-12-18 05:38:33 +0530

    05:38 AM FCM Working

--- app/src/main/java/com/npincomplete/pragyanhackathonamb/MainActivity.java
+++ app/src/main/java/com/npincomplete/pragyanhackathonamb/MainActivity.java
@@ -1,12 +1,18 @@
 package com.npincomplete.pragyanhackathonamb;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -35,7 +41,7 @@ import de.tavendo.autobahn.WebSocketHandler;
 
 import static android.R.id.toggle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     String id;
 
@@ -45,11 +51,21 @@ public class MainActivity extends Activity {
     String latt;
     String longg;
 
+
+
+
+
     String fcm;
     public void btnn(View view)
     {
-        Intent intent = new Intent(this, Webdisplay.class);
-        startActivity(intent);
+
+        progress = new ProgressDialog(this);
+        progress.setTitle("Loading");
+        progress.setMessage("Wait while loading...");
+        progress.setCancelable(false);
+        progress.show();
+        new MasterTask().execute();
+
     }
 
     private static final String TAG = "de.tavendo.test1";
@@ -84,10 +100,36 @@ public class MainActivity extends Activity {
         }
     }
 
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        this.finish();
+        return true;
+
+    }
+    boolean cameFromNotification = false;
+    Button btn;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button)findViewById(R.id.btnnn);
+
+        if (getIntent().getExtras() != null) {
+            Bundle b = getIntent().getExtras();
+            cameFromNotification = b.getBoolean("fromNotification");
+        }
+
+
+
+        //toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //
+        //    setSupportActionBar(toolbar);
+
+
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
 
         tracker = new GPSTracker(this);
         SharedPreferences prefs = getSharedPreferences("dbb", MODE_PRIVATE);
@@ -121,21 +163,6 @@ public class MainActivity extends Activity {
         //new MasterTask().execute();
 
     }
-/*
-
-    JSONObject json = new JSONObject();
-    try {
-        json.put("Id", id);
-        json.put("Lat", tracker.getLatitude());
-        json.put("Long", tracker.getLocation());
-    }
-    catch(JSONException j)
-    {
-
-    }
-    mConnection.sendTextMessage(json.toString());
-*/
-
 
 
     private class TickerAsync extends AsyncTask<String, Void, String> {
@@ -169,7 +196,7 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(String result) {
             //  progress.dismiss();
-            Toast.makeText(getApplicationContext(), outputresponse, Toast.LENGTH_SHORT).show();
+          //  Toast.makeText(getApplicationContext(), outputresponse, Toast.LENGTH_SHORT).show();
             new TickerAsync().execute();
         }
 
@@ -224,9 +251,11 @@ public class MainActivity extends Activity {
                 try
                 {
                     json.put("Id", Integer.parseInt(id));
+/*
                     json.put("Lat", Double.toString(tracker.getLatitude()));
                     json.put("Long", Double.toString(tracker.getLongitude()));
                     json.put("Status", status);
+*/
                 }
                 catch(JSONException e)
                 {
@@ -265,12 +294,73 @@ public class MainActivity extends Activity {
         }
         @Override
         protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            //Toast.makeText(getApplicationContext(), "Posted GPS", Toast.LENGTH_SHORT).show();
-            new MasterTask().execute();
-            //tv.setText("Sending Data every 5 seconds...      Latest Lat Long is (" + tracker.getLatitude() +  ", "+tracker.getLongitude() + ")");
+            progress.dismiss();
+        }
+    }
+    ProgressDialog progress;
+
+        private class dropPatient extends AsyncTask<String, Integer, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    json = new JSONObject();
+                    try
+                    {
+                        json.put("Id", Integer.parseInt(id));
+
+                         }
+                    catch(JSONException e)
+                    {
+                        Log.d("error", "jsonexception");
+                    }
+
+                    Log.d("in async", json.toString());
+                    try {
+                        URL url = new URL("http://02a4ba0f.ngrok.io/amb/update");
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("POST");
+                        connection.setDoOutput(true);
+                        connection.setRequestProperty("Content-Type", "application/json");
+                        OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
+                        osw.write(String.format( String.valueOf(json)));
+                        osw.flush();
+                        osw.close();
+
+
+                        InputStream stream = connection.getInputStream();
+                        InputStreamReader isReader = new InputStreamReader(stream );
+                        BufferedReader br = new BufferedReader(isReader );
+                        outputresponse = br.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "";
+            }
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                //Toast.makeText(getApplicationContext(), "Posted GPS", Toast.LENGTH_SHORT).show();
+                new MainActivity.MasterTask().execute();
+                //tv.setText("Sending Data every 5 seconds...      Latest Lat Long is (" + tracker.getLatitude() +  ", "+tracker.getLongitude() + ")");
+            }
+
         }
 
-    }
 
-    }
+
+
+
+}
--- app/src/main/java/com/npincomplete/pragyanhackathonamb/MyFirebaseMessagingService.java
+++ app/src/main/java/com/npincomplete/pragyanhackathonamb/MyFirebaseMessagingService.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -18,10 +19,34 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
 
-        sendNotification(remoteMessage.getNotification().getBody());
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        super.onMessageReceived(remoteMessage);
+        GPSTracker tracker = new GPSTracker(this);
+
+
+        Intent intent = new Intent(
+                android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?" +
+                        "saddr=" +
+                        remoteMessage.getData().get("Vehicle_Lat") +
+                        "," +
+                        remoteMessage.getData().get("Vehicle_Long") +
+                        "&daddr=" +
+                        remoteMessage.getData().get("Lat") +
+                        "," +
+                        remoteMessage.getData().get("Long")
+                ));
+
+        String temp = "You are picking " + remoteMessage.getData().get("Name") + " for the cause of " +
+                remoteMessage.getData().get("Updated_Description")
+                + " with Phone number " + remoteMessage.getData().get("Phone");
+        sendNotification(temp);
+
+        //PHONE, NAME, TYPE, UDPATED_DESC
+        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+        intent.putExtra("fromNotification", true);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
     }
 
     private void sendNotification(String messageBody) {
@@ -30,18 +55,23 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
-       /* Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
-                .setContentTitle("FCM Message")
-                .setContentText(messageBody)
+                .setSmallIcon(R.drawable.ic_icon2)
+                .setContentTitle("Details")
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(messageBody))
+               // .addAction(R.drawable.common_google_signin_btn_icon_dark, "Drop Patient", pIntent)
+                ;
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 *//* ID of notification *//*, notificationBuilder.build());*/
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        //* ID of notification *//*, notificationBuilder.build());*/
     }
 }
